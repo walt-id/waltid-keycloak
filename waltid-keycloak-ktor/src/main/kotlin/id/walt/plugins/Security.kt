61cf7f23fbfcbd1f78449a1df45cc3a64a41caab
SuperBatata <khaled.battiche@esprit.tn>
2024-01-23 00:10:42 +0100

    added signout function

--- waltid-keycloak-ktor/src/main/kotlin/id/walt/plugins/Security.kt
+++ waltid-keycloak-ktor/src/main/kotlin/id/walt/plugins/Security.kt
@@ -2,7 +2,9 @@ package id.walt.plugins
 
 import com.auth0.jwk.JwkProviderBuilder
 import io.ktor.client.*
+import io.ktor.client.call.*
 import io.ktor.client.engine.apache.*
+import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
@@ -11,13 +13,19 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.sessions.*
 
+import io.ktor.client.request.*
+import io.ktor.client.request.forms.*
+import io.ktor.util.*
+
+
 // based on https://medium.com/@chendenny/ktor-with-oauth-2-0-443b7367c508
 
-val jwkEndpointUrl = URLBuilder("").build().toURI().toURL()
+val jwkEndpointUrl = URLBuilder("http://0.0.0.0:8080/realms/waltid-keycloak-nuxt/protocol/openid-connect/certs").build().toURI().toURL()
 val jwkProvider = JwkProviderBuilder(jwkEndpointUrl).build()
 
-val issuer = "http://127.0.0.1:8080/realms/waltid-keycloak-ktor" // ???
+val issuer = "http://0.0.0.0:8080/realms/waltid-keycloak-nuxt" // ???
 
+@OptIn(InternalAPI::class)
 fun Application.configureSecurity() {
     authentication {
         oauth("keycloakOAuth") {
@@ -26,10 +34,10 @@ fun Application.configureSecurity() {
                 // https://api.ktor.io/ktor-server/ktor-server-plugins/ktor-server-auth/io.ktor.server.auth/-o-auth-server-settings/index.html
                 OAuthServerSettings.OAuth2ServerSettings(
                     name = "keycloak",
-                    authorizeUrl = "http://localhost:8080/realms/waltid-keycloak-ktor/protocol/openid-connect/auth",
-                    accessTokenUrl = "http://localhost:8080/realms/waltid-keycloak-ktor/protocol/openid-connect/token",
-                    clientId = "client-ktor",
-                    clientSecret = "",
+                    authorizeUrl = "http://0.0.0.0:8080/realms/waltid-keycloak-nuxt/protocol/openid-connect/auth",
+                    accessTokenUrl = "http://0.0.0.0:8080/realms/waltid-keycloak-nuxt/protocol/openid-connect/token",
+                    clientId = "waltid_backend",
+                    clientSecret = "5FXJ9IxtMTHWfGUDDU8LGZXaWEu3Qqnk",
                     accessTokenRequiresBasicAuth = false,
                     requestMethod = HttpMethod.Post,
                     defaultScopes = listOf("roles")
@@ -54,21 +62,6 @@ fun Application.configureSecurity() {
                 call.respond(HttpStatusCode.Unauthorized, "Token is not valid or has expired")
             }
         }
-        /*        oauth("auth-oauth-google") {
-            urlProvider = { "http://localhost:8080/callback" }
-            providerLookup = {
-                OAuthServerSettings.OAuth2ServerSettings(
-                    name = "google",
-                    authorizeUrl = "https://accounts.google.com/o/oauth2/auth",
-                    accessTokenUrl = "https://accounts.google.com/o/oauth2/token",
-                    requestMethod = HttpMethod.Post,
-                    clientId = System.getenv("GOOGLE_CLIENT_ID"),
-                    clientSecret = System.getenv("GOOGLE_CLIENT_SECRET"),
-                    defaultScopes = listOf("https://www.googleapis.com/auth/userinfo.profile")
-                )
-            }
-            client = HttpClient(Apache)
-        }*/
     }
     routing {
 
@@ -78,29 +71,49 @@ fun Application.configureSecurity() {
             }
             get("/") {
                 val principal: OAuthAccessTokenResponse.OAuth2? = call.principal()
-                call.respondText("$principal?.accessToken")
+
+                call.respondText("Hello, ${principal?.accessToken} , refresh token ${principal?.refreshToken}!")
+
             }
             authenticate("auth-jwt") {
                 get("/world") {
                     val principal = call.principal<JWTPrincipal>()
                     val username = principal!!.payload.getClaim("preferred_username").asString()
                     val expiresAt = principal.expiresAt?.time?.minus(System.currentTimeMillis())
+
                     call.respondText("Hello, $username! Token is expired at $expiresAt ms. ${principal?.payload?.subject}")
+                }
+                get("/signout") {
+                    val client = HttpClient()
+
+                    val realm = "waltid-keycloak-nuxt"
+                    val logoutUrl = "http://0.0.0.0:8080/realms/$realm/protocol/openid-connect/logout"
+
+
+                    val response: HttpResponse = client.post(logoutUrl) {
+                        header("Authorization", "Bearer ${call.principal<OAuthAccessTokenResponse.OAuth2>()?.accessToken}")
+                        header("Content-Type", "application/x-www-form-urlencoded")
+                        body = FormDataContent(Parameters.build {
+                            append("client_id", "waltid_backend")
+                            call.principal<OAuthAccessTokenResponse.OAuth2>()?.refreshToken?.let { it1 ->
+                                append("refresh_token",
+                                    it1
+                                )
+                            }
+                        })
+                    }
+
+                    val status = response.bodyAsText()
+                    call.respondText("Logout status: $status")
+
+
+
                 }
             }
 
-            /*        authenticate("auth-oauth-google") {
-            get("login") {
-                call.respondRedirect("/callback")
-            }
 
-            get("/callback") {
-                val principal: OAuthAccessTokenResponse.OAuth2? = call.authentication.principal()
-                call.sessions.set(UserSession(principal?.accessToken.toString()))
-                call.respondRedirect("/hello")
-            }
-        }*/
+
         }
     }
 }
-class UserSession(accessToken: String)
+
