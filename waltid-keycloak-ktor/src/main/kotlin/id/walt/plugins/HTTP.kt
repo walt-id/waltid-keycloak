578af7f6408e98b47614caac3def5163b250265e
SuperBatata <khaled.battiche@esprit.tn>
2024-01-30 00:32:40 +0100

    configure cors for ktor

--- waltid-keycloak-ktor/src/main/kotlin/id/walt/plugins/HTTP.kt
+++ waltid-keycloak-ktor/src/main/kotlin/id/walt/plugins/HTTP.kt
@@ -3,9 +3,49 @@ package id.walt.plugins
 import io.ktor.server.application.*
 import io.ktor.server.plugins.swagger.*
 import io.ktor.server.routing.*
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.autohead.*
+import io.ktor.server.plugins.cors.*
+fun CORSConfig.anyHeader(includeUnsafe: Boolean = false) {
+    HttpHeaders.safeHeader.forEach { allowHeader(it) }
+    if (includeUnsafe)
+        HttpHeaders.unsafeHeader.forEach { allowHeader(it) }
+}
 
+val HttpHeaders.safeHeader
+    get() = allHeaders.filter { !isUnsafe(it) }
+
+val HttpHeaders.unsafeHeader
+    get() = allHeaders.filter { isUnsafe(it) }
+
+val HttpHeaders.allHeaders
+    get() = listOf(
+        Accept, AcceptCharset, AcceptEncoding, AcceptLanguage, AcceptRanges,
+        Age, Allow, ALPN, AuthenticationInfo, Authorization, CacheControl, Connection,
+        ContentDisposition, ContentEncoding, ContentLanguage, ContentLength, ContentLocation,
+        ContentRange, ContentType, Cookie, DASL, Date, DAV, Depth, Destination, ETag, Expect,
+        Expires, From, Forwarded, Host, HTTP2Settings, If, IfMatch, IfModifiedSince,
+        IfNoneMatch, IfRange, IfScheduleTagMatch, IfUnmodifiedSince, LastModified,
+        Location, LockToken, Link, MaxForwards, MIMEVersion, OrderingType, Origin,
+        Overwrite, Position, Pragma, Prefer, PreferenceApplied, ProxyAuthenticate,
+        ProxyAuthenticationInfo, ProxyAuthorization, PublicKeyPins, PublicKeyPinsReportOnly,
+        Range, Referrer, RetryAfter, ScheduleReply, ScheduleTag, SecWebSocketAccept,
+        SecWebSocketExtensions, SecWebSocketKey, SecWebSocketProtocol, SecWebSocketVersion,
+        Server, SetCookie, SLUG, StrictTransportSecurity, TE, Timeout, Trailer,
+        TransferEncoding, Upgrade, UserAgent, Vary, Via, Warning, WWWAuthenticate,
+        AccessControlAllowOrigin, AccessControlAllowMethods, AccessControlAllowCredentials,
+        AccessControlAllowHeaders, AccessControlRequestMethod, AccessControlRequestHeaders,
+        AccessControlExposeHeaders, AccessControlMaxAge, XHttpMethodOverride, XForwardedHost,
+        XForwardedServer, XForwardedProto, XForwardedFor, XRequestId, XCorrelationId
+    )
+
+fun CORSConfig.anyMethod() = HttpMethod.DefaultMethods.forEach { allowMethod(it) }
 fun Application.configureHTTP() {
-    routing {
-        swaggerUI(path = "openapi")
+    install(AutoHeadResponse)
+    install(CORS) {
+        anyHost()
+        anyHeader()
+        anyMethod()
     }
 }
