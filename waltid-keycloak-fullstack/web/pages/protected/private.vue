cba1707ab6a4b157667bbf1f0034c3acd740a912
Philipp Potisk <phil@walt.id>
2024-01-30 19:18:05 +0100

    Added fullstack example

---
+++ waltid-keycloak-fullstack/web/pages/protected/private.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="text-center mt-10">
+    <h1 class="text-2xl font-bold">
+      I am only available for authenticated users! with an email  : {{email}}  🎉
+    </h1>
+  </div>
+</template>
+
+
+<script lang="ts" setup>
+
+
+import {sendRequest} from "~/composables/authentification";
+import {SessionProvider ,getSession , useSession ,} from "next-auth/react";
+const session = await getSession()
+
+const email = ref()
+
+
+definePageMeta({auth: true})
+
+async function world() {
+
+  // @ts-ignore
+    await sendRequest(session.accessToken).then((res)=>{
+      email.value = res
+    })
+
+}
+
+
+world()
+</script>
+
